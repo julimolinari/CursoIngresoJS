d5246effb69c9f2b2cdf348081cbc8224571ef0c
alumno <alumno>
2019-02-05 19:53:06 -0300

    guia completa

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,7 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var importe = parseInt(document.getElementById("importe").value);
+
+document.getElementById("resultado").value = importe - ((importe * 25) / 100);
 }
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,7 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	nombre = document.getElementById("elNombre").value;
+	var nombre = document.getElementById("elNombre").value;
     alert(nombre);
 }
 
