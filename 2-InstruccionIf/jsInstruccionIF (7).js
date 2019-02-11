a94918d8879f59b2b3edfdd227f95a1d19713390
julimolinari <43993491+julimolinari@users.noreply.github.com>
2019-02-11 10:50:15 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,7 +1,13 @@
 function mostrar()
 {
 //tomo la edad  
+var edad = document.getElementById("edad").value;
+var estado = document.getElementById("estadoCivil").value;
 
+if	(edad<18 && estado != "Soltero")
+{
+	alert("Es muy pequeño para NO ser soltero.")
+}
 	
 
 
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,7 +1,13 @@
 function mostrar()
 {
-//tomo la edad  
+//tomo la edad   
+	var edad = document.getElementById("edad").value;
+	var estado = document.getElementById("estadoCivil").value;
 
+	if	(edad >= 18 && estado == "Soltero")
+	{
+		alert("Es soltero y no es menor.")
+	}
 	
 
 
