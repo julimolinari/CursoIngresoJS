094672f5bb4921ef97716d9d47c3c2d3415d48b0
julimolinari <43993491+julimolinari@users.noreply.github.com>
2019-02-25 16:52:08 -0300

    Add files via upload

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,73 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+	 var edad;
+	 var sexo;
+	 var estado;
+	 var sueldo;
+	 var legajo;
+	 var nacionalidad;
+	 
+	
+	edad = parseInt (prompt("Ingrese edad: "));
+	
+	
+	
+	while(edad < 18 || edad > 90 || isNaN(edad)){
+		
+		alert("Ingrese edad válida");
+		edad = prompt("Ingrese edad: ");
+				
+	}document.getElementById("Edad").value = edad;	
+	
+	
+	
+	
+	sexo = prompt("Ingrese su sexo (F ó M): ");
+
+	while(sexo != "M" && sexo!="F"){
+		alert("Sexo no valido");
+		sexo= prompt("Ingrese un sexo válido(F ó M): ");
+		}
+
+		document.getElementById('Sexo').value=sexo;
+		
+		
+	
+	estado = parseInt(prompt("Ingrese estado cívil (1 -para soltero, 2 -para casados, 3 -para divorciados y 4 -para viudos): "));
+	
+	
+	while(estado>4 || isNaN(estado)){
+		alert("Estado cívil inválido.");
+		estado = parseInt(prompt("Ingrese estado cívil válido (1 -para soltero, 2 -para casados, 3 -para divorciados y 4 -para viudos): "));
+		
+	}
+	
+	switch(estado){
+		case 1:
+		document.getElementById("EstadoCivil").value = "Soltero";
+		break;
+		
+		case 2:
+		document.getElementById("EstadoCivil").value = "Casado";
+		break;
+		
+		case 3:
+		document.getElementById("EstadoCivil").value = "Divorciado";
+		break;
+		
+		case 4:
+		document.getElementById("EstadoCivil").value = "Viudo";
+		break;
+		
+	}
+	
+	sueldo =parseInt(prompt("Ingrese su sueldo bruto: "));
+	
+	while(sueldo<8000 || isNaN(sueldo)){
+		alert("Sueldo inválido");
+		sueldo =parseInt(prompt("Ingrese su sueldo bruto válido: "));
+		
+	}
+	document.getElementById("Sueldo").value= sueldo;
 }
