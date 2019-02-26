456d442976113e7411676607ef9a1831d674795d
julimolinari <julietamolinari85@gmail.com>
2019-02-26 20:29:17 -0300

    ult clase

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,63 @@
 function mostrar()
 {
+  //SWITCH
+
+  var precio;
+  var pago;
+  var paquete;
+
+  precio = prompt("Precio habitación: ");
+  precio = parseInt(precio);
+
+  pago = prompt("Ingrese metodo de pago: Visa - Paypal - MercadoPago - Efectivo - Otro");
+
+
+  switch (pago){
+    case "Visa":
+    case "MercadoPago":
+    precio = precio - (precio*10/100);
+    alert("El precio con 10% menos es " + precio);
+    break;
+
+    case "PayPal":
+    precio = precio - (precio*15/100);
+
+    paquete= prompt("Ingrese paquete: A-todoIncluido/B-soloDesayunos/C-Ninguno");
+    switch(paquete){
+      case "A":
+      precio = precio - (precio*15/100);
+      break;
+
+    }
+    alert("El precio con descuentoes " + precio);
+    break;
+
+    case "Efectivo":
+    precio = precio - (precio*20/100);
+    paquete= prompt("Ingrese paquete: A-todoIncluido/B-soloDesayunos/C-Ninguno");
+
+    switch(paquete){
+      case "B":
+      precio = precio - (precio*10/100);
+      break;
+
+      case "A":
+      precio = precio - (precio*15/100);
+      break;
+
+    }
+
+    alert("El precio con descuentoes " + precio);
+    break;
+
+    case "Otro":
+    precio = precio - (precio*5/100);
+    alert("El precio con 5% menos es " + precio);
+    break;
+
+
+
+  }
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,16 +1,96 @@
 
 function mostrar()
 {
-    var mascota1 = prompt("Ingrese nombre mascota: ");
+
+  var import1;
+  var import2;
+  var import3;
+  var import4;
+  var suma;
+
+
+
+  import1= prompt("Ingrese primer valor: ");
+  import1 = parseInt(import1);
+
+  import2= prompt("Ingrede segundo valor: ");
+  import2=parseInt(import2);
+
+  import3=prompt("Ingrese tercer valor: ");
+  import3=parseInt(import3);
+
+  import4 = prompt("Ingrese cuarto valor: ");
+  import4=parseInt(import4);
+
+
+  if (import1>=import2 && import1 >= import3 && import1 >= import4){
+    alert(import1 + " es el número mas grande (primer importe)");
+  }else{
+    if(import2>import3 && import2>import4){
+      alert(import2 + " es el número mas grande (segundo importe)");
+    }else{
+      if(import3 >import4){
+        alert(import3 + " es el número mas grande (tercero importe)");
+      }else{
+        alert(import4 + " es el número mas grande (cuarto importe)");
+      }
+    }
+  }
+     suma = import1 + import2 +  import3 + import4;
+
+     if (suma > 100){
+       descuento = suma - ((10*suma)/100)
+       alert ("El precio " + suma + " supera los 100, se hace 10% de descuento y queda en " + descuento);
+     }else{
+      if (suma > 50){
+        descuento = suma - ((5*suma)/100)
+        alert ("El precio " + suma + " supera los 50, se hace 5% de descuento y queda en " + descuento);
+      }
+
+     }
+
+
+}
+/*
+
+var mascota1 = prompt("Ingrese nombre mascota: ");
     var peso1 = parseInt(prompt("Ingrese su peso: "));
 
     var mascota2 = prompt("Ingrese nombre de la otra mascota: ");
     var peso2 = parseInt(prompt("Ingrese su peso: "));
 
 
-    
+
 
      var pesoTotal = peso1 + peso2;
 
     alert("Tenes dos mascotas " + mascota1 + " y "+mascota2+ ", que pesan " + peso1 + " y "+ peso2 + " la suma de los kilos es "+ pesoTotal);
+
+
+*/
+
+
+
+7/*
+
+otra forma de hacerlo
+
+con variable mayor
+
+var mayor= import1;
+
+if(import2>mayor){
+  mayor=import2;
 }
+
+if(import3>mayor){
+  mayor=import3;
+}
+
+if(import4>mayor){
+  mayor=import4;
+}
+
+
+
+*/
